9583d1c455dfaa8a8890d41737f32edce0c4b450
Athikom Vorapattanapong <athikom@mailchannels.com>
2019-01-16 00:07:50 -0800

    Created MVC Project structure

--- src/main/java/com/athikom/streamlabsdemo/streamlabsdemo/StreamlabsdemoApplication.java
+++ src/main/java/com/athikom/streamlabsdemo/streamlabsdemo/StreamlabsdemoApplication.java
@@ -1,14 +1,19 @@
 package com.athikom.streamlabsdemo.streamlabsdemo;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.Configuration;
 
-@SpringBootApplication
+@Configuration
+@EnableAutoConfiguration
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 public class StreamlabsdemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(StreamlabsdemoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(StreamlabsdemoApplication.class, args);
+    }
 
 }
 
