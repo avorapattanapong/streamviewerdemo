709396da9d42e5086247b6d5cc4da06cf9f3dc7e
Athikom Vorapattanapong <athikom@mailchannels.com>
2019-01-15 23:04:39 -0800

    initial commit. framework files

---
+++ src/main/java/com/athikom/streamlabsdemo/streamlabsdemo/StreamlabsdemoApplication.java
@@ -0,0 +1,14 @@
+package com.athikom.streamlabsdemo.streamlabsdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreamlabsdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StreamlabsdemoApplication.class, args);
+	}
+
+}
+
