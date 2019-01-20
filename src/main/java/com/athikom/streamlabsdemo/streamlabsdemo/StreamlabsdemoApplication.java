cad922a8e1b5c73875e61c57c5020c7dfb348e06
Athikom Vorapattanapong <athikom@mailchannels.com>
2019-01-19 16:35:40 -0800

    Home page is working

--- src/main/java/com/athikom/streamlabsdemo/streamlabsdemo/StreamlabsdemoApplication.java
+++ src/main/java/com/athikom/streamlabsdemo/streamlabsdemo/StreamlabsdemoApplication.java
@@ -3,12 +3,11 @@ package com.athikom.streamlabsdemo.streamlabsdemo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @EnableAutoConfiguration
-@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
+@SpringBootApplication
 public class StreamlabsdemoApplication {
 
     public static void main(String[] args) {
