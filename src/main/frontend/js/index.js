a8d59c0cda2119f2e431845ebf1acb1b14c97009
Athikom Vorapattanapong <athikom@mailchannels.com>
2019-01-20 23:13:32 -0800

    React router is setup

--- src/main/frontend/js/index.js
+++ src/main/frontend/js/index.js
@@ -1,15 +1,27 @@
-console.log("Hello");
-
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
+import { Route } from 'react-router-dom'
+import Paragraph from "./components/paragraph";
 
 class App extends React.Component {
     render() {
-        return <p>Testing Working {this.props.message}</p>;
-
+        return(
+            <div id="nav">
+                <NavLink exact to="/one">One</NavLink>
+                <NavLink exact to="/two">Two</NavLink>
+                <div className="content">
+                    <Route exact path="/one" render={()=> <Paragraph message="paragraphOne"/>} />
+                    <Route exact path="/two" render={()=> <Paragraph message="paragraphTwo"/>} />
+                </div>
+            </div>
+        )
     }
 }
-
 let app = document.getElementById("app");
 
-ReactDOM.render(<App message="thisisworking"/>, app);
+ReactDOM.render(
+    <BrowserRouter>
+        <App/>
+    </BrowserRouter>, app);
